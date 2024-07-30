efa72455a1ff3ac6c2adaf537851389c5cd6fc2b
ArthurCunha-Dev <arthurcunha1114@gmail.com>
2024-07-29 22:29:58 -0300

    .

--- src/App.js
+++ src/App.js
@@ -4,23 +4,29 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Cadastrar from './components/pages/Cadastrar';
+
+
 
 function App() {
   return (
     <Router>
       <div>
-      <Link to="/">Home</Link>
-      <Link to="/contact">Contato</Link>
-      <Link to="/company">Empresa</Link>
-      <Link to="/newproject">Novo Projeto</Link>
-        <li>Home</li>
-        <li>Contato</li>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link> 
+        <Link to="/cadastrar">Cadastrar</Link>        
+
       </div>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/company" element={<Company />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/newproject" element={<NewProject />} />
+        <Route path='/cadastrar' element={<Cadastrar/>} />
+
       </Routes>
       <p>Footer</p>
     </Router>

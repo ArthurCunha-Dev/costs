19c111293d594b749102bbb2b6a50db92ac5a8f7
ArthurCunha-Dev <arthurcunha1114@gmail.com>
2024-08-09 19:10:35 -0300

    add o Project Form e o Link Button

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import ProjectForm from '../project/ProjectForm'; // Verifique se o caminho está correto
+import styles from './NewProject.module.css'; // Certifique-se de que o caminho está correto
 
-
-function NewProject (){
-    return(
-        <h1> NewProject</h1>
-    )
+function NewProject() {
+    return (
+        <div className={styles.newproject_container}>
+            <h1>New Project</h1>
+            <p>Crie seu projeto para depois adicionar serviços</p>
+            <ProjectForm />
+        </div>
+    );
 }
 
 export default NewProject;

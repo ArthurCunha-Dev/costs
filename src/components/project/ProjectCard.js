457b5536e82985bd7789b50ce5747d44493d5574
ArthurCunha-Dev <arthurcunha1114@gmail.com>
2024-08-23 18:19:59 -0300

    add o Project Card

---
+++ src/components/project/ProjectCard.js
@@ -0,0 +1,16 @@
+import styles from './ProjectCard.module.css';
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs';
+
+function ProjectCard({ id, name, budget, category, handleRemove }) {
+    return (
+        <div className={styles.projectCard}>
+            <h2>{name}</h2>
+            <p>Orçamento: {budget}</p>
+            <p>Categoria: {category}</p>
+            <button onClick={() => handleRemove(id)}><BsFillTrashFill /> Remover</button>
+            <button><BsPencil /> Editar</button>
+        </div>
+    );
+}
+
+export default ProjectCard;

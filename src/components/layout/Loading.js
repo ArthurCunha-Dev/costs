5384c8bcb6e68cc542ae9c59cf87db3ff66a15ee
ArthurCunha-Dev <arthurcunha1114@gmail.com>
2024-09-04 19:57:12 -0300

    alterações no Loading

--- src/components/layout/Loading.js
+++ src/components/layout/Loading.js
@@ -3,6 +3,11 @@ import loading from '../../img/loading.png'
 import styles from './Loading.module.css'
 
 function Loading() {
+    return(
+        <div className={styles.loader_container}>
+          <img className={styles.loader} src={loading} alt="Loading" />
+        </div>
+    )
 
 }
 
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,13 +4,16 @@ import { useLocation } from 'react-router-dom'
 
 import Message from '../layout/Message'
 import Container from '../layout/Container'
+
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../project/ProjectCard'
 
 import styles from './Projects.module.css'
+import Loading from '../layout/Loading'
 
 function Projects (){
  const [projects, setProjects] = useState([])
+ const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
     let message = ''
@@ -19,19 +22,24 @@ function Projects (){
     }
         
     useEffect(() => {
-      fetch  ("http://localhost:5000/projects", {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-            console.log (data.category)
-            setProjects(data);
-        })
-        .catch((err) => console.log(err));
- }, [])
+        setTimeout(
+            () =>{
+                fetch  ("http://localhost:5000/projects", {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                    },
+                })
+                .then((resp) => resp.json())
+                .then((data) => {
+                    console.log (data.category)
+                    setProjects(data);
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err));
+            }, 3000)
+      
+    }, [])
     
     return(
     <div className={styles.project_container}>
@@ -53,6 +61,7 @@ function Projects (){
                     />
                 ))
             )}
+            {!removeLoading && <Loading/>}
         </Container>
         <div className={styles.project_container}>
           <LinkButton to="/newproject" text="Criar Projeto" />
